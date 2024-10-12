0395c9133b1079ab1e9dab773fe383d58e5ea708
Auroali <dawn.auroali@gmail.com>
2024-10-12 17:10:28 -0300

    built in fix that changes item ids, more examples, example world

--- src/test/java/com/auroali/dfuhooks/test/SchemaModifyTest.java
+++ src/test/java/com/auroali/dfuhooks/test/SchemaModifyTest.java
@@ -1,15 +1,25 @@
 package com.auroali.dfuhooks.test;
 
 import com.auroali.dfuhooks.DFUHooksSchemaHook;
+import com.auroali.dfuhooks.builtinfixes.DFUHooksItemIdFix;
+import com.google.common.collect.ImmutableMap;
 import com.mojang.datafixers.DataFixerBuilder;
 
 public class SchemaModifyTest implements DFUHooksSchemaHook {
     @Override
     public void modifySchemas(DataFixerBuilder builder, SchemaGetter schemas) {
-        System.out.println("Test");
-        schemas.fromVersion(99).ifPresent(schema -> {
-            System.out.println("Found schema for dataversion 99");
-            System.out.println(schema.getVersionKey());
+        // get the schema for version 3818 subversion 5
+        // this is the schema used for the item stack componentization fix
+        schemas.fromVersion(3818, 5).ifPresent(schema -> {
+            // adds a fixer that replaces all diamonds with emeralds
+            builder.addFixer(new DFUHooksItemIdFix(
+                    schema,
+                    "dfuhooks-test",
+                    ImmutableMap
+                            .<String, String>builder()
+                            .put("minecraft:diamond", "minecraft:emerald")
+                            .build()
+            ));
         });
 
     }
