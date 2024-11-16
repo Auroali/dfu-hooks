03b05165f1dbd71bd783073a04959a1efb62de06
Auroali <dawn.auroali@gmail.com>
2024-11-15 21:18:05 -0400

    reworked how entities and block entities are registered

---
+++ src/main/java/com/auroali/dfuhooks/DFUHooks.java
@@ -0,0 +1,9 @@
+package com.auroali.dfuhooks;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DFUHooks {
+    public static SchemaRegistry SCHEMA_REGISTRY;
+    public static final Logger LOGGER = LoggerFactory.getLogger("DFU Hooks");
+}
--- src/main/java/com/auroali/dfuhooks/mixin/SchemasMixin.java
+++ src/main/java/com/auroali/dfuhooks/mixin/SchemasMixin.java
@@ -1,6 +1,8 @@
 package com.auroali.dfuhooks.mixin;
 
+import com.auroali.dfuhooks.DFUHooks;
 import com.auroali.dfuhooks.DFUHooksSchemaHook;
+import com.auroali.dfuhooks.SchemaRegistry;
 import com.mojang.datafixers.DataFixerBuilder;
 import com.mojang.datafixers.schemas.Schema;
 import it.unimi.dsi.fastutil.ints.Int2ObjectSortedMap;
@@ -14,8 +16,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(Schemas.class)
 public abstract class SchemasMixin {
 
+    @Inject(method = "build", at = @At("HEAD"))
+    private static void dfuhooks$initSchemaRegistry(DataFixerBuilder builder, CallbackInfo ci) {
+        DFUHooks.SCHEMA_REGISTRY = new SchemaRegistry();
+        FabricLoader.getInstance().invokeEntrypoints("dfuhooks-modify-schemas", DFUHooksSchemaHook.class, hook -> hook.register(DFUHooks.SCHEMA_REGISTRY));
+    }
+
     @Inject(method = "build", at = @At("TAIL"))
     private static void dfuhooks$registerAndModifySchemas(DataFixerBuilder builder, CallbackInfo ci) {
+        // print a message if we failed to register anything
+        DFUHooks.SCHEMA_REGISTRY.validate();
+        // we don't need the schema registry anymore
+        DFUHooks.SCHEMA_REGISTRY = null;
+
         Int2ObjectSortedMap<Schema> schemas = ((DataFixerBuilderAccessor)builder).getSchemas();
         FabricLoader.getInstance().invokeEntrypoints(
                 "dfuhooks-modify-schemas",
